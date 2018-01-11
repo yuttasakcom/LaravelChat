05f6d29631d00b8f34ea0c6cc84145de327218ad
YoProgrammer <yuttasakcom@gmail.com>
2018-01-11 21:21:42 +0700

    update resource views pages layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <title><?=$title?></title>
 </head>
 <body>
-    <div id="app">
-    <p>ทดสอบ Laravel Event Listener</p>
-    </div>
-    <script src="js/app.js"></script>
+    @yield('content')
 </body>
 </html>
--- routes/admin.php
+++ routes/admin.php
@@ -2,15 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Admin Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register admin routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "admin" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('pages.home')->with('title', 'Home Page');
+    echo 'Hello Admin!';
 });

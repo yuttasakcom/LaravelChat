abaeb55dd39228d86af8d78e75e5a65b3ce9e076
YoProgrammer <yuttasakcom@gmail.com>
2018-01-12 10:56:19 +0700

    update layouts/header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,10 @@
 <header class="bg-dark">
-
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark container">
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">

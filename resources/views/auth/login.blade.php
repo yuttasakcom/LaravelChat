16b01b6aa7ea0db0ac76de2cb5e1b1d91a372d51
YoProgrammer <yuttasakcom@gmail.com>
2018-01-14 19:32:43 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,40 @@
 @extends('layouts.login') @section('content')
 <div class="login">
+    <div class="login-container mt-5">
+        <div class="input-container mt-5">
+            <form>
+                <h2>Login to your account</h2>
+                <p>
+                    <label for="loginUsername">Username</label>
+                    <input type="email" placeholder="e.g. example@gmail.com">
+                </p>
 
+                <p>
+                    <label for="loginPassword">Password</label>
+                    <input type="password" placeholder="Your password">
+                </p>
+
+                <button type="submit" name="loginButton">LOG IN</button>
+
+                <div class="has-account-text">
+                    <span id="hideLogin">Don't have an account yet?
+                        <a href="/register">Signup here.</a>
+                    </span>
+                </div>
+            </form>
+
+        </div>
+
+        <div class="login-text mt-5">
+            <h1>Laravel Music</h1>
+            <h2>Listen to loads of songs for free</h2>
+            <ul>
+                <li>Discover music you'll fall in love with</li>
+                <li>Create your own playlists</li>
+                <li>Follow artists to keep up to date</li>
+            </ul>
+        </div>
+
+    </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.login') @section('content')
+<div class="login">
+    <div class="login-container">
+        <div class="input-container">
+            <form>
+                <h2>Create your free account</h2>
+                <p>
+                    <label for="username">Username</label>
+                    <input id="username" name="username" type="text" placeholder="e.g. bartSimpson">
+                </p>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <p>
+                    <label for="firstName">First name</label>
+                    <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart">
+                </p>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+                <p>
+                    <label for="lastName">Last name</label>
+                    <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson">
+                </p>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                <p>
+                    <label for="email">Email</label>
+                    <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com">
+                </p>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                <p>
+                    <label for="email2">Confirm email</label>
+                    <input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com">
+                </p>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                <p>
+                    <label for="password">Password</label>
+                    <input id="password" name="password" type="password" placeholder="Your password" required>
+                </p>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                <p>
+                    <label for="password2">Confirm password</label>
+                    <input id="password2" name="password2" type="password" placeholder="Your password" required>
+                </p>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                <button type="submit" name="registerButton">SIGN UP</button>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="has-account-text">
+                    <span id="hideRegister">Already have an account?
+                        <a href="/login">Log in here.</a>
+                    </span>
                 </div>
-            </div>
+
+            </form>
         </div>
+
+        <div class="login-text">
+            <h1>Laravel Music</h1>
+            <h2>Listen to loads of songs for free</h2>
+            <ul>
+                <li>Discover music you'll fall in love with</li>
+                <li>Create your own playlists</li>
+                <li>Follow artists to keep up to date</li>
+            </ul>
+        </div>
+
     </div>
 </div>
 @endsection

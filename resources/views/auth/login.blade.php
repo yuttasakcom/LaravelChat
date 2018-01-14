a9a0ccba6fca5fc88e6f221084a71daeee48622f
YoProgrammer <yuttasakcom@gmail.com>
2018-01-14 19:51:11 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,17 +2,39 @@
 <div class="login">
     <div class="login-container mt-5">
         <div class="input-container mt-5">
-            <form>
+            <form method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+
                 <h2>Login to your account</h2>
                 <p>
-                    <label for="loginUsername">Username</label>
-                    <input type="email" placeholder="e.g. example@gmail.com">
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="loginUsername">Username</label>
+                        <input type="email" name="email" placeholder="e.g. example@gmail.com" required autofocus> @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
+                    </div>
                 </p>
 
                 <p>
-                    <label for="loginPassword">Password</label>
-                    <input type="password" placeholder="Your password">
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="loginPassword">Password</label>
+                        <input type="password" name="password" placeholder="Your password" required> @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
+                    </div>
                 </p>
+
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember" {{ old( 'remember') ? 'checked' : '' }}> Remember Me
+                        </label>
+                    </div>
+                </div>
 
                 <button type="submit" name="loginButton">LOG IN</button>
 
